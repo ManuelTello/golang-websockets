a804ae565308f1501877e1cb22c2ae7ca6f387ee
ManuelTello <manuelTello.business@gmail.com>
2023-08-17 13:53:10 -0300

    Modulo de reader agregado
    Se creo el modulo reader para las conexiones entrantes donde se controla el life cycle de esta.

--- reader.go
+++ reader.go
@@ -3,19 +3,46 @@ package main
 import (
 	"github.com/gorilla/websocket"
 	"fmt"
+	"encoding/json"
 )
 
 func Reader(connection *websocket.Conn){
 	for{
-		_,msg,_ := connection.ReadMessage()
-		
-		for i:=0;i<cm.size;i++ {
-			
+		msgtype,msg,msgerr := connection.ReadMessage()
+
+		if msgerr != nil{
+			fmt.Printf("Error on message: %s\n",msgerr)
 		}
-		
-		for i:=0;i<cm.size;i++ {
-			//cm.conns[i].WriteMessage(msgt,msg)
-			fmt.Print(string(msg))		
+
+		var usrm Client
+		err:=json.Unmarshal(msg,&usrm)
+	
+		if err != nil{
+			fmt.Printf("Error parsing json: %s\n",err)
+		}
+
+		var user_index = IdentifyClientByUsername(usrm.username)
+
+		if user_index != -1 {
+			var user_copy Client = clients.clients[user_index]
+			user_copy.username = usrm.username
+			user_copy.color = usrm.color
+			user_copy.px = usrm.px
+			user_copy.py = usrm.py
+			clients.clients[user_index] = user_copy
+		}else{			
+			clients.clients[clients.size] = Client{
+				username:usrm.username,
+				color:usrm.color,
+				px:usrm.px,
+				py:usrm.py,
+			}
+			clients.size = clients.size + 1
+		}				
+
+		for i:=0;i<connections.size;i++ {
+			data,_ := json.Marshal(clients.clients)
+			connections.conns[i].WriteMessage(msgtype,data)
 		}
 	}
 }
